c034cdb3f49ab540b1255725272c3a86793e9a64
Калинин Артем Геннадьевич <35222@rendez-vous.ru>
2019-05-23 19:02:57 +0300

    start

---
+++ src/components/StarWarsSeacrh.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import store from '../store';
+
+
+class StarWarsSearch extends Component {
+
+    render() {
+        return (
+            <div className="component">
+               
+            </div>
+        )
+    }
+};
+
+export default StarWarsSearch;
